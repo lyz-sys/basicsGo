452ea44cda27d8042d5d796b9a43d9856edeb067
lyz-sys <linyuzheng0607@gmail.com>
2021-09-22 16:15:51 +0800

    feat台阶-走法

--- takeStep.go
+++ takeStep.go
@@ -7,18 +7,25 @@ import (
 
 type intnum uint64
 
-func main() {
-	fmt.Println(time.Now())
-	var n intnum = 50
-	fmt.Println(n.t())
-	fmt.Println(time.Now())
-}
-
-func (n intnum) t() uint64 {
+func (n intnum) t() intnum {
 	if n == 1 {
 		return 1
 	} else if n == 2 {
 		return 2
 	}
 	return (n - 1).t() + (n - 2).t()
+}
+
+func main() {
+	var choice intnum
+	fmt.Print("请输入台阶数量:")
+	_, err := fmt.Scanln(&choice)
+	if err != nil {
+		fmt.Println("无效的选项")
+		return
+	}
+	fmt.Printf("你选择了:%d\n", choice)
+	fmt.Println(time.Now())
+	fmt.Println(choice.t())
+	fmt.Println(time.Now())
 }
